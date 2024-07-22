3727487b586a44337bd73074566b68d22b42a41f
Adipony <purvanjaro2@gmail.com>
2024-07-23 01:30:07 +0300

    init

---
+++ .prettierrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+	plugins: [require.resolve('prettier-plugin-tailwindcss')],
+	tabWidth: 4,
+	printWidth: 110,
+	useTabs: true,
+	semi: true,
+	singleQuote: true,
+	quoteProps: 'consistent',
+	trailingComma: 'all',
+	bracketSpacing: true,
+	arrowParens: 'always',
+	bracketSameLine: false,
+};
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors: {
+        'w98-background': '#c0c0c0'
+      }
+    },
+  },
+  plugins: [],
+}
+
