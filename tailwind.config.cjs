63255ec55226cb2540f9494497431868c01350d8
Adipony <purvanjaro2@gmail.com>
2024-12-06 04:12:55 +0200

    add 'post' page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,11 +3,13 @@ module.exports = {
 	content: ['./src/**/*.{html,js,css}'],
 	theme: {
 		colors: {
-			'coffee-100': '#ece0d1',
-			'coffee-200': '#dbc1ac',
-			'coffee-300': '#967259',
-			'coffee-400': '#634832',
-			'coffee-500': '#38220f',
+			coffee: {
+				100: '#ece0d1',
+				200: '#dbc1ac',
+				300: '#967259',
+				400: '#634832',
+				500: '#38220f',
+			}
 		},
 		extend: {
 			boxShadow: {
